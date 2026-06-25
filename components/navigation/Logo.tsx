7d3c40576689c60ab55bdbbf4cde9792459be94e
Verghese George Keenalil <verghese.keenalil@gmail.com>
2026-06-25 08:40:36 +0000

    Logo not visible error, wrong file extension

--- components/navigation/Logo.tsx
+++ components/navigation/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Logo = ({ w, h }: { w: number; h: number }) => {
 	return (
 		<Image
-			src="/images/logo.webp"
+			src="/images/logo.png"
 			width={w}
 			height={h}
 			alt="NMMUN logo"
